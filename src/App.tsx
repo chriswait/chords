07ffadb70580dad35b40b5427ebbc8f46b1536b8
chriswait <chriswait91@gmail.com>
2024-12-22 17:23:31 +0000

    basic stuff working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,103 @@
-import "./App.css";
+import Chord from "@tombatossals/react-chords/lib/Chord";
+import guitar from "@tombatossals/chords-db/lib/guitar";
+import ukulele from "@tombatossals/chords-db/lib/ukulele";
+import { useState } from "react";
+
+const instruments = { ukulele, guitar };
 
 function App() {
+  const [instrumentName, setInstrumentName] = useState("ukulele");
+  const selectedInstrument = instruments[instrumentName];
+  const instrumentWithTunings = Object.assign(selectedInstrument.main, {
+    tunings: selectedInstrument.tunings,
+  });
+  const [root, setRoot] = useState("C");
+  const selectedChords = selectedInstrument.chords[root];
+
+  const [showAllChordTypes, setShowAllChordTypes] = useState(false);
+  const [chordTypes, setChordTypes] = useState([
+    "major",
+    "minor",
+    "maj7",
+    "7",
+    "m7",
+  ]);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <div style={{ marginBottom: 10 }}>
+        {Object.keys(instruments).map((eachInstrumentName) => (
+          <button
+            key={eachInstrumentName}
+            onClick={() => setInstrumentName(eachInstrumentName)}
+            style={
+              instrumentName === eachInstrumentName
+                ? { fontWeight: "bold" }
+                : {}
+            }
+          >
+            {eachInstrumentName}
+          </button>
+        ))}
+      </div>
+      <div style={{ marginBottom: 10 }}>
+        {selectedInstrument.keys.map((keyRoot: string) => (
+          <button
+            key={keyRoot}
+            onClick={() => setRoot(keyRoot)}
+            style={root === keyRoot ? { fontWeight: "bold" } : {}}
+          >
+            {keyRoot}
+          </button>
+        ))}
+      </div>
+      {showAllChordTypes ? (
+        <>
+          <div>
+            {selectedInstrument.suffixes.map((suffix: string) => (
+              <button
+                key={suffix}
+                onClick={() => setChordTypes([suffix])}
+                style={
+                  chordTypes.includes(suffix) ? { fontWeight: "bold" } : {}
+                }
+              >
+                {suffix}
+              </button>
+            ))}
+          </div>
+          <button onClick={() => setShowAllChordTypes(false)}>
+            Hide Chords
+          </button>
+        </>
+      ) : (
+        <button onClick={() => setShowAllChordTypes(true)}>Show Chords</button>
+      )}
+      <h2>{root}</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(5, auto)",
+        }}
+      >
+        {selectedChords
+          .filter((chord) => chordTypes.includes(chord.suffix))
+          .map((chord) => {
+            return (
+              <div key={chord.key + chord.suffix}>
+                <h2>{chord.suffix}</h2>
+                {chord.positions.map((position, index) => (
+                  <Chord
+                    key={`position-${index}`}
+                    chord={position}
+                    instrument={instrumentWithTunings}
+                    lite={false}
+                  />
+                ))}
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 }
